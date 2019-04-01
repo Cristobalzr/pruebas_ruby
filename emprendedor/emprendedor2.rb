6b3c4613c931c223486f3ef56c79f7c98607c484
Cristobal <cristobalz@outlook.com>
2019-04-01 20:53:24 -0300

    pruebas con ruby arreglo emprendedor 2.1

--- emprendedor/emprendedor2.rb
+++ emprendedor/emprendedor2.rb
@@ -1,7 +1,9 @@
 p = ARGV[0].to_f
+p_premium = 2
+p_free = 0
 users = ARGV[1].to_i
 premium_users = ARGV[2].to_i
 free_users = ARGV[3].to_i
 g = ARGV[4].to_f
-uti = p * (users + premium_users * 2) - g
+uti = p * (users + premium_users * p_premium + free_users * p_free) - g
 puts uti
