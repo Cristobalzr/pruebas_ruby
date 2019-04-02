506f27e2e0a4d0d9cbdedf358379f97a09bc6369
Cristobal <cristobalz@outlook.com>
2019-04-01 22:31:30 -0300

    pruebas con ruby arreglo emprendedor con impuesto

--- emprendedor/emprendedor1.rb
+++ emprendedor/emprendedor1.rb
@@ -2,4 +2,8 @@ p = ARGV[0].to_f
 users = ARGV[1].to_i
 g = ARGV[2].to_f
 uti = p * users - g
-puts  uti
+
+if uti > 0
+  uti_tax = uti * 0.65
+  puts uti_tax
+end
--- emprendedor/emprendedor2.rb
+++ emprendedor/emprendedor2.rb
@@ -6,4 +6,8 @@ premium_users = ARGV[2].to_i
 free_users = ARGV[3].to_i
 g = ARGV[4].to_f
 uti = p * (users + premium_users * p_premium + free_users * p_free) - g
-puts uti
+
+if uti > 0
+  uti_tax = uti * 0.65
+  puts uti_tax
+end
