d493a731f00fe2a2555aa732e6007e7cc3b1146d
Cristobal <cristobalz@outlook.com>
2019-04-03 20:11:22 -0300

    Agrego nulo a cuarto valor cuando no se ingresa

--- mayor/mayor_de_3.rb
+++ mayor/mayor_de_3.rb
@@ -2,7 +2,6 @@ first = ARGV[0].to_f
 second = ARGV[1].to_f
 third = ARGV[2].to_f
 
-
 if first > second && first > third
   puts first
 elsif second > third
--- mayor/mayor_de_4.rb
+++ mayor/mayor_de_4.rb
@@ -1,16 +1,27 @@
 first = ARGV[0].to_f
 second = ARGV[1].to_f
 third = ARGV[2].to_f
-fourth = ARGV[3].to_f
 
+fourth = ARGV[3]
 
-
-if first > second && first > third && first > fourth
-  puts first
-elsif second > third && second > fourth
-  puts second
-elsif third > fourth
-  puts third
+if fourth == nil
+  if first > second && first > third && first
+    puts first
+  elsif second > third && second
+    puts second
+  else
+    puts third
+  end
 else
-  puts fourth
+  fourth = fourth.to_f
+  if first > second && first > third && first > fourth
+    puts first
+  elsif second > third && second > fourth
+    puts second
+  elsif third > fourth
+    puts third
+  else
+    puts fourth
+  end
+
 end
