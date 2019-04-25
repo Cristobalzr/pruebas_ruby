3120f725f598ea3fc0f0f7a66bfa3176b6029f9f
Cristobal <cristobalz@outlook.com>
2019-04-24 21:40:25 -0400

    ident

--- hashes/api/apinasa.rb
+++ hashes/api/apinasa.rb
@@ -5,15 +5,15 @@ require 'json'
 require 'openssl'
 
 def request(address, api_key)
-url = URI("#{address}&api_key=#{api_key}")
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-request = Net::HTTP::Get.new(url)
-request["Cache-Control"] = 'no-cache'
-request["Postman-Token"] = '50c7548a-44dd-4279-847d-ef8315a18672,4bcaa907-d22b-4f0a-aa02-ff6b4a5a1137'
-response = http.request(request)
-JSON.parse(response.read_body)
+  url = URI("#{address}&api_key=#{api_key}")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(url)
+  request["Cache-Control"] = 'no-cache'
+  request["Postman-Token"] = '50c7548a-44dd-4279-847d-ef8315a18672,4bcaa907-d22b-4f0a-aa02-ff6b4a5a1137'
+  response = http.request(request)
+  JSON.parse(response.read_body)
 end
 
 data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10", "7kzGYbdCHEa5iIiripMu3YMaOS2UNxAUIHJxJXAL")
